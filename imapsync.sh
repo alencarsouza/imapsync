b59d2215b0939b92f5ec1e7ef49d01e4d2ebf574
Junior Holowka <junior.holowka@gmail.com>
2016-12-07 11:16:51 +0100

    Updated with new version

--- imapsync.sh
+++ imapsync.sh
@@ -4,7 +4,7 @@
 # imapsync-list - standard list of accounts (need move to /tmp folder)
 #
 #
-# Copyright (c) 2013 Junior Holowka <junior.holowka@gmail.com>
+# Copyright (c) 2013-2017 Junior Holowka <junior.holowka@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,32 +24,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
 # check for root
 if [ "$(id -u)" != "0" ]; then
    echo "You must run this script as root" 1>&2
    exit 1
 fi
 
+if [ ! -f /usr/local/imapsync ]; then
 
 # installing dependencies
- apt-get update
- apt-get install libdigest-hmac-perl libdigest-hmac-perl libterm-readkey-perl \
-				libterm-readkey-perl libdate-manip-perl libdate-manip-perl libmail-imapclient-perl \
-				makepasswd rcs perl-doc libmail-imapclient-perl make
+apt-get update -y
+apt-get install -y libauthen-ntlm-perl libcrypt-ssleay-perl libdigest-hmac-perl \
+libfile-copy-recursive-perl libio-compress-perl libio-socket-inet6-perl \
+libio-socket-ssl-perl libio-tee-perl libmodule-scandeps-perl libnet-ssleay-perl \
+libpar-packer-perl libreadonly-perl libterm-readkey-perl libtest-pod-perl \
+libtest-simple-perl libunicode-string-perl liburi-perl make cpanminus git
 
-# cloning and installing imapsync
- git clone git://github.com/imapsync/imapsync.git
- cd imapsync/
- make install
+cpanm Data::Uniqid Mail::IMAPClient
+cpanm Authen::NTLM Data::Uniqid File::Copy::Recursive IO::Tee Mail::IMAPClient Unicode::String
 
+# install imapsync
+mkdir -pv /opt/
+(cd /opt/ && git clone git://github.com/imapsync/imapsync.git)
+cp /opt/imapsync/imapsync /usr/local/bin
 
+}
 
 # running
 cat imapsync-list|grep -v "^#" | sed '/^ *$/d' > /tmp/imapsync-list
 
 LIST="/tmp/imapsync-list"
-SERVER1="mx1.domain.com"
+SERVER1="imap.yourwebmail.com"
 SERVER2="imap.gmail.com"
 HIDE="--nofoldersizes --skipsize" #Blank this out if you want to see folder sizes
 
@@ -65,15 +70,7 @@ while IFS=: read UNAME1 PWORD1 UNAME2 PWORD2; do
 	--port2 993 --user2 ${UNAME2} \
 	--password2 ${PWORD2} --ssl2 \
 	--authmech1 LOGIN --authmech2 LOGIN --split1 200 --split2 200 ${HIDE} \
-	--exclude 'Drafts|Trash|Spam|Sent'
+	--exclude 'Drafts|Trash|Spam'
 
 done < ${LIST}
-
-
-
-
-
-
-
-
 
